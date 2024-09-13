900c9aeb0eb9fa1f6d629f3e396bcfca60435b2e
TamaChandra <zaidchandradityawiratama@mail.ugm.ac.id>
2024-09-13 11:40:19 +0700

    Rev 1

--- PemrogramanTLS.cpp
+++ PemrogramanTLS.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <cmath>
 using namespace std;
+char confirm;
 int x;
 int y;
 int z;
@@ -8,6 +9,15 @@ float hasil;
 
 int main(){
     cout <<"Mencari Panjang Vektor 3 Dimensi     \n";
+    cout <<"Apakah anda ingin melanjutkan? (Y/N)\n";
+    cin>>confirm;
+    
+if (confirm == 'N' || confirm == 'n') {
+        cout << "Program dibatalkan\n";
+        cout << "----------------------------------------------\n";
+        return 0; // Keluar dari program jika pengguna memilih 'N' atau 'n'
+    }
+
     cout <<"--------------------------------------\n";
     cout <<"Masukan X: ";
     cin>>x;
@@ -18,5 +28,6 @@ int main(){
     hasil=sqrt(x*x+y*y+z*z);
     cout <<"--------------------------------------\n";
     cout <<"Panjang Vektor = "<<hasil;
+
 return 0;
 }
